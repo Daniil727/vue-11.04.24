e19af0250ed78a940404c2861a3cb3251753a24a
Daniil <danechka.serebrov@mail.ru>
2024-04-12 21:04:13 +0300

    work 11.04.24

---
+++ src/components/v-cart-item.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="v-cart-item">
+    <img class="v-catalog-item-img-cart" :src="cart_item_data.imgURL" :alt="cart_item_data.image">
+    <div>
+      <p> {{ cart_item_data.name }}</p>
+      <p> {{ cart_item_data.price }}</p>
+      <p> {{ cart_item_data.article }}</p>
+    </div>
+    <div class="v-cart-item-quantity">
+      <p>Quantity</p>
+      <span>{{ cart_item_data.quantity }}</span>
+    </div>
+    <button @click="deleteFromCart">Delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"v-cart-item",
+    props: {
+      cart_item_data:{
+        tape: Object,
+        default(){
+          return [];
+        }
+      }
+    },
+    data(){
+        return {
+          title:"hello"
+        };
+    },
+    methods: {
+      deleteFromCart () {
+        this.$emit('deleteFromCart')
+      }
+    },
+    watch:{},
+      mounted() {
+      console.log("карта добавлена")
+    },
+    computed: {},
+    
+
+}
+</script>
+
+<style>
+.v-cart-item {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    flex-basis: 25%;
+    align-items: center;
+    box-shadow: 0 0 8px 0 gray;
+    padding: 20px;
+    margin: 20px;
+}
+.v-catalog-item-img-cart {
+  /* display: flex;
+  justify-content: center;
+  align-items: center; */
+  height: 80%;
+  width: 80%;
+}
+
+</style>
---
+++ src/components/v-cart.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="v-cart">
+    <h2 class="v-cart-h2">cart</h2> 
+    <vCartItem 
+    v-for="(item, index) in cart_data" 
+    :key="item.article" 
+    :cart_item_data="item"
+    @deleteFromCart="deleteFromCart(index)"/>
+  </div>
+</template>
+
+<script>
+import vCartItem from "./v-cart-item.vue";
+import { mapActions } from "vuex";
+
+
+export default {
+    name:"v-cart",
+    components:{
+      vCartItem
+      },
+    props: {
+      cart_data: {
+        type: Array,
+        default(){
+          return []
+        }
+
+      }
+    },
+    data(){
+        return {
+           
+        };
+    },
+    methods: {
+      ...mapActions(["DELETE_FROM_CART"]),
+      deleteFromCart(index){
+        this.DELETE_FROM_CART(index)
+      },
+    },
+    computed: {},
+    
+}
+</script>
+
+<style>
+.v-cart{
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  color: blue;
+  font-size: 20px;
+  margin-bottom: 50px;
+}
+.v-cart-h2 {
+  text-align: center;
+  font-size: 20px;
+}
+</style>
